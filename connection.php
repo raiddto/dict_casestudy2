7717762c8e21081663067065fd4bb5a3406c7533
errjury <earl.jerryt@gmail.com>
2022-08-26 16:47:28 +0800

    000webhost config

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'dict');
+define('DB_USER','id19474749_peo_cloud_db_user');
+define('DB_PASS' ,'Y|bCM51X9\z{%RDu');
+define('DB_NAME', 'id19474749_peo_cloud_db');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
